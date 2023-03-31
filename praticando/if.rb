853eef069a9dd13f10b4b16b1d343f5c34de05a5
Matheus M Almeida <matheus.mma2015@gmail.com>
2023-03-30 23:03:37 -0300

    praticando loop

---
+++ praticando/if.rb
@@ -0,0 +1,39 @@
+if 2 < 4
+    puts 'é Real!'
+end
+
+puts 'Certo!' if 1 < 4
+
+
+if 4 < 3
+    puts "Legal é menor"
+else
+    puts "Menor"
+end
+
+ataque_terra = true
+ataque_agua = false
+
+if ataque_terra == true
+    puts "Solte o bicho"
+elsif ataque_agua == true
+    puts "Nao solte"
+else
+    puts "Todo mundo corre"
+end
+
+grade = 'F'
+
+case grade
+when 'A'
+  puts "You're a genius"
+  future_bank_account_balance = 5_000_000
+when 'D'
+  puts "Better luck next time"
+  can_i_retire_soon = false
+else
+  puts "'YOU SHALL NOT PASS!' -Gandalf"
+  fml = true
+end
+
+puts("woah") || true
---
+++ praticando/loop.rb
@@ -0,0 +1,13 @@
+i = 0
+
+loop do
+    puts "i is #{i}"
+    i += 1
+    break if i == 10
+end
+
+a = 0
+while a < 10 do
+    puts a 
+    a += 1
+end
