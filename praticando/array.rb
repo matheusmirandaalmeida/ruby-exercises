e216bc97f6d895cfe471b701c9f74b3949deff89
Matheus M Almeida <matheus.mma2015@gmail.com>
2023-03-31 22:55:19 -0300

    loop

---
+++ praticando/array.rb
@@ -0,0 +1,23 @@
+# Descreva o que é um array e explique por que ele é útil.
+    # Array é uma coleção, podendo ser de str ou numb, é versátil por poder manipular coleções de dados. 
+
+# Descreva duas maneiras diferentes de criar um novo array.
+
+    array_type = [1, 2, 3]
+    Array.new(1, 6)
+
+# Explique como acessar elementos de matriz usando métodos diferentes.
+
+    puts array_type[0] # Irá retornar o primeiro indice
+
+# Explique três maneiras diferentes de adicionar dados a uma matriz.
+
+    array_type.push(3, 6) # adiciona os elementos especificados
+    array_type << 2  # adiciona por último elemento.
+    array_type.unshift # adiciona como primeiro elemento
+
+# Explique como remover elementos de uma matriz.
+
+    puts array_type.pop #  POP remove o último elemento
+    array_type.shift # remove o primeiro elemento
+
--- praticando/loop.rb
+++ praticando/loop.rb
@@ -11,3 +11,32 @@ while a < 10 do
     puts a 
     a += 1
 end
+
+i = 0
+until i >= 10 do
+ puts "i is #{i}"
+ i += 1
+end
+
+5.times do
+    puts "Hello, world!"
+  end
+
+while gets.chomp != "yes" do
+    puts "Voce vai sair comigo?"
+    if gets.chomp == "yes"
+        puts 'legal!'
+        break
+    end
+end
+
+5.times do |number|
+    puts "Alternative fact number #{number}"
+  end
+
+
+  5.upto(10) {|num| print "#{num} " }     #=> 5 6 7 8 9 10
+
+  10.downto(5) {|num| print "#{num} " }   #=> 10 9 8 7 6 5
+  
+# Se você precisar percorrer uma série de números (ou mesmo letras) dentro de um intervalo específico, esses são os loops para você.
